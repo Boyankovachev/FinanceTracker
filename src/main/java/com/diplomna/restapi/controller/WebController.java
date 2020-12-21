34cd0e3bfd98c4e8112e113a85080115e23c99c4
Boyan Kovachev <boyankovachev40@gmail.com>
2020-12-21 17:48:54 +0200

    e

--- src/main/java/com/diplomna/restapi/controller/WebController.java
+++ src/main/java/com/diplomna/restapi/controller/WebController.java
@@ -4,7 +4,10 @@ import com.diplomna.restapi.service.LoginAndRegister;
 import com.mysql.cj.xdevapi.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WebController {
@@ -22,8 +25,28 @@ public class WebController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void getLogin(@RequestBody String inputString){
-        System.out.println(loginAndRegister.verifyLogin(inputString));
+    public RedirectView getLogin(@RequestBody String inputString, /*Model model */ RedirectAttributes attributes){
+        String result = loginAndRegister.verifyLogin(inputString);
+        /*
+        if(result.equals("Successful login!")){
+            return "redirect:/test";
+        }
+        else return "redirect:/test2";
+
+         */
+        /*
+        if(result.equals("Successful login!")){
+            model.addAttribute("message", "pone uceli parolata");
+        }
+        else model.addAttribute("message", "ne uceli parolata");;
+        return "redirect:/logintest";
+
+         */
+        if(result.equals("Successful login!")){
+            attributes.addAttribute("message", "login successful");
+        }
+        else attributes.addAttribute("message", "login failed");
+        return new RedirectView("logintest");
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
@@ -36,11 +59,10 @@ public class WebController {
         System.out.println(loginAndRegister.createUser(inputString));
     }
 
-
-    @RequestMapping("/in")
-    @ResponseBody
-    public String pedal(){
-        return "you are succesfully logged in";
+    @RequestMapping(value = "/logintest", method = RequestMethod.GET)
+    public String getTest(Model model, @RequestParam String message){
+        model.addAttribute("message", message);
+        return "logintest";
     }
 
 }
